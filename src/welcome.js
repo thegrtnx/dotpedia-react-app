b6d95c048f3684cd68fef67b548f292ede45aad0
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-02-19 20:59:11 +0100

    update state

--- src/welcome.js
+++ src/welcome.js
@@ -8,6 +8,11 @@ const Signup = () => {
                 <div className="px-4 my-5 justify-content-center text-center pt-1">
                     <div className="text-start">
                         <img src={welcomesvg} alt="DotPedia" className="img-fluid" width="90"/>
+                        <h1 className="text-white display-1 mb-4 mt-lg-4 mt-3 col-lg-10">Padii mi, aowfar?</h1>
+                        <p style={{color: "#a1a1a1"}} className="mb-2 col-lg-9">No need to remember a passwords or pin. We'll use your email address to perform the magic in seconds! 🚀</p>
+                        <form method="post" enctype="application/x-www-form-urlencoded">
+                            
+                        </form>
                     </div>
                 </div>
             </div>
