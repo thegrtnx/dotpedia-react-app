aa3b1dac56ea1f52840f9202bf8edc4c470f7617
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-02-19 20:45:36 +0100

    signup screen

---
+++ src/welcome.js
@@ -0,0 +1,19 @@
+import welcomesvg from './img/appimg/padi.png';
+
+const Signup = () => {
+    return ( 
+
+        <section className="pt-1 mt-1 m-auto">
+            <div className="container col-lg-6">
+                <div className="px-4 my-5 justify-content-center text-center pt-1">
+                    <div className="text-start">
+                        <img src={welcomesvg} alt="DotPedia" className="img-fluid" width="90"/>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+     );
+}
+ 
+export default Signup;
